0271e1734fc92eb637c092fcb3cdb15cb904151c
SeungBum Kim <picxenk@gmail.com>
2016-11-21 22:35:29 +0900

    test

---
+++ JavaS.js
@@ -0,0 +1,4 @@
+var answer = prompt("이름은?")
+if (answer == "수지"){
+    alert("안녕");
+}
---
+++ Javalang.java
@@ -0,0 +1,8 @@
+class nSum {
+    pubilc static void main(String[] arg){
+        int i=0; int sum=0;
+        for(i=1; i<=10; i++)
+            sum=sum+i;
+        System.out.println(sum);
+    }
+}
